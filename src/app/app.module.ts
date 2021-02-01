2388bb97a8567c5d6459f692613bce32023f8c4d
AnnKemp <ann.kemp@scarlet.be>
2021-02-01 20:16:54 +0100

    routing toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
